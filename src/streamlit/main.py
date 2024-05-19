ea763270da798a84b4816949e97b99e407c57ab3
Toma <ulti4929@gmail.com>
2024-05-19 04:31:59 +0000

    modified:   src/streamlit/main.py 	modified:   src/streamlit/main.py 	src/creator/yoshinosan/abstractor.py 	src/creator/yoshinosan/concretizer.py

--- src/streamlit/main.py
+++ src/streamlit/main.py
@@ -18,7 +18,24 @@ tab1, tab2, tab3 = st.tabs(["Creator", "Coder", "Evaluator"])
 with tab1:
     st.markdown('## Creator')
     st.markdown('新規手法の作成')
-    st.markdown('こちらに吉野さんの実行画面を作成していただきたいです！')
+    txt_OBJECTIVE = st.text_input('目的', placeholder='ここにどのように改善してほしいのか目的を記入', max_chars=1000)
+
+    st.markdown('1. 初期値の入力')
+    tab1_1, tab1_2, tab1_3 = st.tabs(["M_pre初期値", "M_post初期値", "その他初期値"])
+    with tab1_1:
+        txt_M_pre_name = st.text_input('M_preの名前', placeholder='ここにM_preの手法名を記入', max_chars=100)
+        txt_M_pre_code = st.text_area(
+        'M_pre', placeholder='もとになる対象手法M_preのPythonコードを入力', height=150, max_chars=10000
+        )
+    with tab1_2:
+        txt_M_patch_name = st.text_input('M_patchの名前', placeholder='ここにM_patchの手法名を記入', max_chars=100)
+        txt_M_patch_code = st.text_area(
+        'M_patch', placeholder='もとになる対象手法M_patchのPythonコードを入力', height=150, max_chars=10000
+        )
+    with tab1_3:
+        reference_python_code = st.text_area(
+        'reference_python', placeholder='参考になるA_B-datasetリポジトリのPythonコードを入力', height=150, max_chars=100000
+        )
 
 
 
